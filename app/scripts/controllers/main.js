effbd0142a5d6c3bce27ddafaa225a1f7c484e9c
gandhiv88 <gandhiv88@gmail.com>
2014-08-09 02:25:18 +0530

    Annyang !!!!
    Added Annyang

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -14,4 +14,37 @@ angular.module('scoreStarApp')
       'AngularJS',
       'Karma'
     ];
+    $scope.hello =function(){
+    	$("#section_hello").css("visibility","visible");
+    	$("#hello").slideDown("slow");
+      	$scope.scrollTo("#section_hello");
+    };
+
+    $scope.hide =function(){
+    	$("#section_hello").css("visibility","hidden");
+    	$("body").slideDown("slow");
+      	$scope.scrollTo("body");
+    };
+
+     $scope.commands = {
+      'start':        $scope.hello,
+      'stop': 		  $scope.hide
+    };
+
+    $scope.scrollTo = function(identifier, speed) {
+    $('html, body').animate({
+        scrollTop: $(identifier).offset().top
+    }, speed || 1000);
+  };
+  // OPTIONAL: activate debug mode for detailed logging in the console
+    annyang.debug();
+
+    // Add voice commands to respond to
+    annyang.addCommands($scope.commands);
+
+    // OPTIONAL: Set a language for speech recognition (defaults to English)
+    annyang.setLanguage('en');
+
+    // Start listening. You can call this here, or attach this call to an event, button, etc.
+    annyang.start();
   });
